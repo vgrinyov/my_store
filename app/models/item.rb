57808826e165e88db820bc0b44879223df091369
vgrinyov <vitaliy.grinyov>
2016-09-25 01:17:13 +0300

    added callbacks to model

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,11 @@ class Item < ApplicationRecord
   validates :price, { numericality: { greater_than: 0, allow_nil: true}}
   validates :name, :description, presence: true
 
+  # callbacks
+
+  after_initialize { puts "initialized"} # Item.new; Item.first
+  after_save { puts "saved"} # Item.save; Item.create; item.update_attributes()
+  after_create { puts "created"}
+  after_update { puts "updated"}
+  after_destroy {puts "destroyed"} # item.destroy
 end
