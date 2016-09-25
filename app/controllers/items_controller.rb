1064b3cc4a0638862682a8fe3beabf61d97bec12
vgrinyov <vitaliy.grinyov>
2016-09-25 19:05:01 +0300

    refactored to rest controller and added crud operations

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,8 +4,35 @@ class ItemsController < ApplicationController
     render text: @items.map { |i| "#{i.name}: #{i.price}"}.join("<br/>")
   end
 
+  # /items/1 GET
+  def show
+  end
+
+  # /items/new  GET
+  def new
+  end
+
+  # /items/1/edit  GET
+  def edit
+  end
+
+  # /items/new  GET
+  def new
+  end
+
+  # /items POST
   def create
     #@item = Item.create(name: params[:name], description: params[:description], price: params[:price], real: params[:real], weight: params[:weight])
     @item = Item.create(params[:item])
     render :plain => "#{@item.id}: #{@item.name} (#{@item.new_record?})"
+  end
+
+  # /items/1 PUT
+  def update
+  end
+
+  # /items/1 DELETE
+  def destroy
+  end
+
 end
