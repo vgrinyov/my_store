2e425c30c2e2b7ee8f1f51a3dcd0e764adf69179
vgrinyov <vitaliy.grinyov@gmail.com>
2016-09-26 17:59:50 +0300

    added realization methods update and edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+
   def index
     @items = Item.all
     render text: @items.map { |i| "#{i.name}: #{i.price}"}.join("<br/>")
@@ -17,35 +18,50 @@ class ItemsController < ApplicationController
   # /items/1 GET var2
   def show
     unless @item = Item.where(id: params[:id]).first
-      render text: "Page no found", status: 404
+    #unless @item = Item.find_by(params[:id])
+      render html: "Page no found", status: 404
     end
   end
 
   # /items/new  GET
   def new
+    @item = Item.new
   end
 
   # /items/1/edit  GET
   def edit
-  end
-
-  # /items/new  GET
-  def new
+    @item = Item.find(params[:id])
   end
 
   # /items POST
   def create
-    #@item = Item.create(name: params[:name], description: params[:description], price: params[:price], real: params[:real], weight: params[:weight])
-    @item = Item.create(params[:item])
-    render :plain => "#{@item.id}: #{@item.name} (#{@item.new_record?})"
+    #params.permit!
+    @item = Item.create(item_params)
+    if @item.errors.empty?
+      redirect_to items_path(@item), :notice => "Your item was saved" # /items/:id
+    else
+      render "new"
+    end
   end
 
   # /items/1 PUT
   def update
+    @item = Item.find(params[:id])
+    @item.update_attributes(params[:item])
+    if @item.errors.empty?
+      redirect_to items_path(@item), :notice => "Your item was saved" # /items/:id
+    else
+      render "edit"
+    end
   end
 
   # /items/1 DELETE
   def destroy
   end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:id, :name, :description, :price, :weight )
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
+
   #attr_accessible :price, :name, :real, :weight, :description
-  attr_accessor :price, :name, :real, :weight, :description
 
   validates :price, { numericality: { greater_than: 0, allow_nil: true}}
   validates :name, :description, presence: true
