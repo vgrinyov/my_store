d1fe530ca232b90f2fd05e3da17cea48228c29ed
vgrinyov <vitaliy.grinyov>
2016-09-27 21:13:10 +0300

    added associations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_one :cart
+  has_many :orders
 end
--- db/migrate/20160927175354_create_orders.rb
+++ db/migrate/20160927175354_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
-
+      t.integer :user_id
       t.timestamps
     end
   end
