08c9da50446309f30305bff3b51df6cee9479e37
vgrinyov <vitaliy.grinyov>
2016-09-27 22:47:50 +0300

    added new model and association

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,7 @@
 class Cart < ApplicationRecord
+
   belongs_to :user
+
+  has_and_belongs_to_many :items
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,9 @@ class Item < ApplicationRecord
   #attr_accessible :price, :name, :real, :weight, :description
 
   validates :price, { numericality: { greater_than: 0, allow_nil: true}}
-  validates :name, :description, presence: true
+  validates :name, presence: true
+
+  has_and_belongs_to_many :carts
 
   # callbacks
 
