160ff06b276668cde9cc64696bcf0a7dac7cd956
vgrinyov <vitaliy.grinyov>
2016-10-01 17:22:39 +0300

    refactored action index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,8 +5,12 @@ class ItemsController < ApplicationController
   before_action :check_if_admin, only: [:edit, :update, :new, :create, :destroy]
 
   def index
-    @items = Item.all
-    #render html: @items.map { |i| "#{i.name}: #{i.price}"}.join("<br/>")
+    @items = Item
+    @items = @items.where("price >= ?", params[:price_from]) if params[:price_from]
+    @items = @items.where("created_at >= ?", 1.day.ago) if params[:today]
+    @items = @items.where("votes_count >= ?", params[:votes_from]) if params[:votes_from]
+    @items = @items.order("votes_count DESC", "price")
+    @items = @items.includes(:image)
   end
 
   def expensive
