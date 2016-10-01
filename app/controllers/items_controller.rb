942cfa836e98844b37efb2d3a8bb7154e3d30948
vgrinyov <vitaliy.grinyov>
2016-10-01 15:31:52 +0300

    refactored code and added new admin controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,36 +1,26 @@
 class ItemsController < ApplicationController
 
   # Filters
-  before_filter :find_item, only: [:show, :edit, :update, :destroy, :upvote]
-  before_filter :check_if_admin, only: [:edit, :update, :new, :create, :destroy]
+  before_action :find_item, only: [:show, :edit, :update, :destroy, :upvote]
+  before_action :check_if_admin, only: [:edit, :update, :new, :create, :destroy]
 
   def index
     @items = Item.all
-    render html: @items.map { |i| "#{i.name}: #{i.price}"}.join("<br/>")
-  end
-
-  # /items/1 GET var1
-=begin
-  def show
-    if @item = Item.where(id: params[:id]).first
-      render "items/show"
-    else
-      render text: "Page no found", status: 404
-    end
-  end
-=end
-  # /items/1 GET var2
-  def show
-    unless @item
-    #unless @item = Item.find_by(params[:id])
-      render html: "Page no found", status: 404
-    end
+    #render html: @items.map { |i| "#{i.name}: #{i.price}"}.join("<br/>")
   end
 
   def expensive
     @item = Item.where("price > 1000")
     render "index"
   end
+
+  # /items/1 GET
+  def show
+    unless @item
+      render html: "Page no found", status: 404
+    end
+  end
+
   # /items/new  GET
   def new
     @item = Item.new
@@ -75,9 +65,11 @@ class ItemsController < ApplicationController
 
   private
 
+=begin
   def item_params
     params.require(:item).permit(:id, :name, :description, :price, :weight )
   end
+=end
 
   def find_item
     @item = Item.where(id: params[:id]).first
