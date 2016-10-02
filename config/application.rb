06765fd6652c0921809dbca31c8bd51af1e8c3b6
vgrinyov <vitaliy.grinyov>
2016-10-02 20:16:09 +0300

    added support i18n

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,13 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+#config.i18n,default_locale = :ru
+
 module MyStore
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
   end
 end
